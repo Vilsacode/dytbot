debd60a5577ca89c78b118ab1c6f5c70937618de
Vilsafur <vilsafur@gmail.com>
2024-11-25 21:00:51 +0000

    style: Formattage du code

--- src/discord/config.ts
+++ src/discord/config.ts
@@ -2,7 +2,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID, DISCORD_CHANNEL_ANNONCE, DISCORD_CHANNEL_BOT } = process.env;
+const { 
+  DISCORD_TOKEN,
+  DISCORD_CLIENT_ID,
+  DISCORD_CHANNEL_ANNONCE,
+  DISCORD_CHANNEL_BOT
+} = process.env;
 
 if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
   throw new Error("Missing environment variables");
