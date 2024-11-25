43c3518763802f878336bc2659d278b9cb5a9cbd
Vilsafur <vilsafur@gmail.com>
2024-11-25 21:00:22 +0000

    refacto: Variabilisation du message

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import { sendMessage } from './discord/action';
 import { config } from './discord/config';
 
 discord.once("ready", () => {
-  console.log("DYTBot bot is ready! 🤖");
+  const startingMessage = "DYTBot is ready! 🤖"
+  console.log(startingMessage);
   if (config.DISCORD_CHANNEL_BOT) {
-    sendMessage(config.DISCORD_CHANNEL_BOT, "DYTBot is ready! 🤖")
+    sendMessage(config.DISCORD_CHANNEL_BOT, startingMessage)
   }
 });
