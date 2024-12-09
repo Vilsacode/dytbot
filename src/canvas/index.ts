7797771a530a7f0364691ecd13c791e12be93c9e
Vilsafur <vilsafur@gmail.com>
2024-12-09 20:20:47 +0000

    feat: Mise en place des images d'annonces et de level up

--- src/canvas/index.ts
+++ src/canvas/index.ts
@@ -1,17 +1,94 @@
-import Canvas from 'canvas'
-import { config } from '../config'
+import Canvas, { Image } from 'canvas'
 
-export const generateStreamLiveCard = async () => {
-  const canvas = Canvas.createCanvas(700, 250)
+export const generateStreamLiveCard = async (titre: string) => {
+  const canvas = Canvas.createCanvas(700, 400)
   const context = canvas.getContext('2d')
 
-  const background = await Canvas.loadImage('./assets/fond_espace.jpg')
-  context.drawImage(background, 0, 0, canvas.width, canvas.height)
+  const background = await Canvas.loadImage('./assets/fond_annonces.jpg')
+  const logo = await Canvas.loadImage('./assets/logo_twitch.png')
+  
+  const avatar_size = 150
 
-  context.font = '28px sans-serif'
-  context.fillStyle = '#d60f34'
+  backgroundImage(context, background, canvas.width, canvas.height)
+  context.drawImage(logo, 30, 30, avatar_size, avatar_size)
 
-  context.fillText(`Le live commence`, 140, 80)
+  context.font = "40px 'Fira Code', serif"
+  context.fillStyle = '#D401CC'
+  context.textAlign = 'end'
+  context.fillText(`Nouveau live en cours`, canvas.width - 20, (canvas.height / 2) + 50)
+
+  context.font = "60px 'Fira Code', serif"
+  context.fillStyle = '#FFFFFF'
+  context.textAlign = 'end'
+  context.fillText(`#${titre}`, canvas.width - 50, canvas.height-30)
 
   return canvas
+}
+
+export const generateYoutubeVideoCard = async (titre: string) => {
+  const canvas = Canvas.createCanvas(700, 400)
+  const context = canvas.getContext('2d')
+
+  const background = await Canvas.loadImage('./assets/fond_annonces.jpg')
+  const logo = await Canvas.loadImage('./assets/logo_youtube.png')
+
+  backgroundImage(context, background, canvas.width, canvas.height)
+  context.drawImage(logo, 30, 30, 143, 100)
+
+  context.font = "40px 'Fira Code', serif"
+  context.fillStyle = '#D401CC'
+  context.textAlign = 'end' 
+  context.fillText(`Nouvelle vidéo disponible`, canvas.width - 20, (canvas.height / 2) + 50)
+
+  context.font = "60px 'Fira Code', serif"
+  context.fillStyle = '#FFFFFF'
+  context.textAlign = 'end'
+  context.fillText(`#${titre}`, canvas.width - 50, canvas.height-30)
+
+  return canvas
+}
+
+export const generateLevelUpCard = async (level: number, username: string, avatar: Image) => {
+  const canvas = Canvas.createCanvas(700, 400)
+  const context = canvas.getContext('2d') 
+
+  const avatar_size = 100
+
+  const background = await Canvas.loadImage('./assets/fond_level_up.jpg')
+  backgroundImage(context, background, canvas.width, canvas.height)
+  context.drawImage(avatar, 120, 10, avatar_size, avatar_size)
+
+  context.font = "60px 'Fira Code', serif"
+  context.fillStyle = '#FF66FF'
+  context.textAlign = 'center'
+  context.fillText(level.toString(), canvas.width / 1.89, canvas.height / 2.05)
+  context.fillStyle = '#FFFFFF'
+  context.fillText(`Level UP`, (canvas.width / 1.95), (canvas.height / 2.15) + 150)
+  
+  context.font = "40px 'Fira Code', serif"
+  context.textAlign = 'end'
+  context.fillText(username, canvas.width - 30, (avatar_size / 2) + 10)
+
+  return canvas
+}
+
+const backgroundImage = (context: Canvas.CanvasRenderingContext2D, background: Image, width: number, height: number) => {
+  let x = 0
+  let y = 0
+  let radius = 40
+  context.save();
+  context.beginPath();
+  context.moveTo(x + radius, y);
+  context.lineTo(x + width - radius, y);
+  context.quadraticCurveTo(x + width, y, x + width, y + radius);
+  context.lineTo(x + width, y + height - radius);
+  context.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+  context.lineTo(x + radius, y + height);
+  context.quadraticCurveTo(x, y + height, x, y + height - radius);
+  context.lineTo(x, y + radius);
+  context.quadraticCurveTo(x, y, x + radius, y);
+  context.closePath();
+  context.clip();
+  context.drawImage(background, 0, 0, width, height)
+  context.restore();
 }
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,20 @@ import { sendImage, sendMessage } from './discord/action';
 import { config } from './config';
 import { startServer } from './server';
 import { start } from './twitch/ws'
-import { generateStreamLiveCard } from './canvas';
+import { generateLevelUpCard, generateStreamLiveCard, generateYoutubeVideoCard } from './canvas';
 
 import './database/migration'
+import { loadImage } from 'canvas';
 discord.once("ready", async () => {
   const startingMessage = "DYTBot is ready! 🤖"
   console.log(startingMessage);
   if (config.DISCORD_CHANNEL_BOT) {
     sendMessage(config.DISCORD_CHANNEL_BOT, startingMessage)
 
-    sendImage(config.DISCORD_CHANNEL_BOT, await generateStreamLiveCard(), `@everyone : Rejoint nous sur twitch : https://www.twitch.tv/${config.TWITCH_CHANNEL_NAME}`)
+    const avatar =  await loadImage('./assets/avatar.png')
+    sendImage(config.DISCORD_CHANNEL_BOT, await generateLevelUpCard(10, 'vilsafur', avatar))
+    sendImage(config.DISCORD_CHANNEL_BOT, await generateStreamLiveCard('DYTBot'))
+    sendImage(config.DISCORD_CHANNEL_BOT, await generateYoutubeVideoCard('DYTBot'))
   }
 });
 
